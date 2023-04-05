d33e67efb8304d23bf9547e9f602d638438484e5
Martin Rudolf <martinwrudolf@gmail.com>
2023-04-05 14:57:44 -0600

    Made important changes

--- mm/settings.py
+++ mm/settings.py
@@ -20,11 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--i^6-zl536xpmjtr@3%+z^y_64woxt%@cv+3+a5+7m#8%n3h_q'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+DEBUG = False
 
 ALLOWED_HOSTS = ['aqueous-scrubland-59263.herokuapp.com', 'mapmind.herokuapp.com']
 
