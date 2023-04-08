b5af111fbfb9fbb02e35ff81c25141ec98cbfe00
Martin Rudolf <martinwrudolf@gmail.com>
2023-04-08 10:16:42 -0600

    Updated

--- mm/settings.py
+++ mm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mapmind.herokuapp.com']
 
