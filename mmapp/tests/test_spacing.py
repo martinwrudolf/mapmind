feedc1cfe841b87185a866f2ce2ea3b77a2d4169
Martin Rudolf <martinwrudolf@gmail.com>
2023-04-12 06:08:57 -0600

    Added spacing alg test. 83% coverage

--- mmapp/tests/test_spacing.py
+++ mmapp/tests/test_spacing.py
@@ -10,3 +10,29 @@ class test_spacing(TestCase):
         for i in range(pos.shape[0]):
             for j in range(pos.shape[1]):
                 self.assertIsNotNone(pos[i][j])
+
+
+from django.test import TestCase
+from numpy.testing import assert_array_equal
+from ..src.spacing import spacing_alg as sp
+
+
+class SimilarityScoresTestCase(TestCase):
+    def test_num_scores_incremented(self):
+        num_words = 5
+        num_scores = 3
+        words = ["apple", "banana", "orange", "peach", "pear"]
+
+        scores = sp.similarity_scores(num_words, num_scores, words)
+
+        self.assertEqual(scores.shape, (num_words, num_scores + 1))
+
+    def test_scores_between_zero_and_one(self):
+        num_words = 5
+        num_scores = 3
+        words = ["apple", "banana", "orange", "peach", "pear"]
+
+        scores = sp.similarity_scores(num_words, num_scores, words)
+
+        self.assertTrue((scores[:, 1:] >= 0).all())
+        self.assertTrue((scores[:, 1:] <= 1).all())
