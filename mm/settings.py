2da9b7fccf420c87d2d16121359bf390c02869d9
Claire Martin <cmmartin@ualberta.ca>
2023-04-12 15:34:58 -0600

    adding settings

--- mm/settings.py
+++ mm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = "ehskelfehio247298454hois"
 
 DEBUG = True
 
